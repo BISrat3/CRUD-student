88f20941db0376bda21075e5a0fd8b3e6c8f1396
bisrat <bisrat.amtataw@gmail.com>
2022-07-02 11:52:12 -0400

    test Nav

---
+++ student-Record-Front-End/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Nav from '../src/component/Nav'
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      {/* <h1>Student Record</h1>
+      <header className="App-header">
+      </header> */}
+    </div>
+  );
+}
+
+export default App;
