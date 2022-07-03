3cfaa9b3ded67469b36ef96f16c98d2510c29d31
bisrat <bisrat.amtataw@gmail.com>
2022-07-03 19:15:14 -0400

    test hello world

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,46 @@
 const express = require('express')
 const mongoose = require ('mongoose') 
-
+const cors = require('cors');
+const morgan = require('morgan');
 const app = express()
 
 require('./config/db.connection')
+
+// Models
+
+const StudentSchema = new mongoose.Schema({
+    fname:String,
+    lname:String,
+    phonenumber:String,
+    stateCode:Number,
+    stateName:String,
+})
+
+
+const Student = mongoose.model ("Student", StudentSchema);
+
+// Middleware
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+
+// Routes 
+
+app.get("/", (req,res)=>{
+    res.send("hello world")
+})
+
+// Index Route
+app.get('/student', async (req,res)=>{
+    try{
+        res.json(await Student.find({}));
+
+    }
+    catch (error){
+        res.status(400).json(error);
+    }
+})
 
 
 app.listen(process.env.PORT, ()=>
