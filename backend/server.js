a12b02ec9c2a1e66b3ad4f57fde6e8fcb2c13122
bisrat <bisrat.amtataw@gmail.com>
2022-07-04 16:31:32 -0400

    student

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const mongoose = require ('mongoose')
 const cors = require('cors');
 const morgan = require('morgan');
 const models = require('./models')
+const controllers = require ('./Controllers')
 const app = express()
 
 require('./config/db.connection')
@@ -15,30 +16,32 @@ app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 
+app.use('/', controllers)
+
 // Routes 
 
 app.get("/", (req,res)=>{
     res.send("hello world")
 })
 
-// Index Route
-app.get('/student', async (req,res)=>{
-    try{
-        res.json(await db.find({}));
+// // Index Route
+// app.get('/student', async (req,res)=>{
+//     try{
+//         res.json(await db.find({}));
 
-    }
-    catch (error){
-        res.status(400).json(error);
-    }
-})
+//     }
+//     catch (error){
+//         res.status(400).json(error);
+//     }
+// })
 
-app.post("/student", async (req,res) =>{
-    try {
-        res,json(await db.create(req.body));
-    } catch (error){
-        res.status(400).json(error);
-    }
-})
+// app.post("/student", async (req,res) =>{
+//     try {
+//         res,json(await db.create(req.body));
+//     } catch (error){
+//         res.status(400).json(error);
+//     }
+// })
 
 app.listen(process.env.PORT, ()=>
     console.log(`Listening on port:${process.env.PORT}`))
